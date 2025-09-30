f34b09f66784cfff0f0da22a84fb2d1ffd4bb3d7
djwilson7 <dontai.j.wilson@gmail.com>
2025-09-30 13:15:39 -0500

    Added the about button in the top left of the screen and adjusted spacing around hero text.

--- src/client/pages/landing/LandingPage.tsx
+++ src/client/pages/landing/LandingPage.tsx
@@ -3,14 +3,18 @@
 // import localComponents from "./landing-components/"
 // import public from "/asset-filename.ext"
 
+import { useNavigate } from "react-router";
 import { LandingForm } from "./landing-components/LandingForm";
+import Button from "@/client/global-components/button";
 
 export function LandingPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex fixed inset-0 justify-center items-center overflow-auto">
       <div className="flex flex-col justify-evenly h-full w-full md:flex-row">
         
-        <div className="flex flex-col h-3/2 md:h-full w-full md:w-1/2 items-center justify-center gap-3">
+        <div className="flex flex-col w-7/8 h-3/2 md:h-full items-center justify-center self-center gap-3">
           <img src="/vite.svg" className="w-32 h-32 mx-auto" />
           <h1>JAICE</h1>
           <div className="text-left">
@@ -19,10 +23,12 @@ export function LandingPage() {
           </div>
         </div>
 
-
         <div className="flex h-1/2 w-7/8 md:h-full w-[34rem] items-center justify-center self-center p-20">
           <LandingForm />
         </div>
+      </div>
+      <div className="fixed top-0 left-0 m-4">
+        <Button onClick={() => navigate("/about")}>About</Button>
       </div>
     </div>
   );
