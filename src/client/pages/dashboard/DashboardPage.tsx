1376da258ebe5a7db158a19f2bc1c80678c531ac
Maya H <107822233+catkus12@users.noreply.github.com>
2025-10-02 18:18:38 -0500

    re added the red button cause i messed it up and also fixed the background on the dashboard page cause too bright

--- src/client/pages/dashboard/DashboardPage.tsx
+++ src/client/pages/dashboard/DashboardPage.tsx
@@ -6,7 +6,8 @@
 export function DashboardPage() {
 
   return (
-    <div className="w-full h-full flex items-center justify-center flex-col gap-4 bg-orange-300">
+    <div className="w-full h-full flex items-center justify-center flex-col gap-4 bg-orange-300"
+    style={{background: "var(--color-bg)"}}>
       <h1 className="text-4xl font-bold">Dashboard Page</h1>
     </div>
   );
