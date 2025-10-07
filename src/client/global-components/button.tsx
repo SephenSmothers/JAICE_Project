d6f644783941a8f956372edb8e71b964b02db682
djwilson7 <dontai.j.wilson@gmail.com>
2025-10-07 17:10:07 -0500

    Updated the landing page.
    LandingPage -> Logo now has a fixed size that doesn't expand or contract with resizing.
    
    button.tsx -> updated to accept a type for form submissions.
    
    FloatingInputField -> updated to contain a tool tip that's centere above the input field. Also updated to react to user input as they type.
    
    input-validation -> added for basic string validation for email/passwords.
    
    landing.api.ts -> added to hold the front end api calls used by the landing page.
    
    login.tsx/signup -> Integrated input validation.
    
    quicksignin -> updated to use landing.api to simulate authentication via third parties.

--- src/client/global-components/button.tsx
+++ src/client/global-components/button.tsx
@@ -4,17 +4,20 @@ export default function Button({
   onClick,
   children,
   isSelected = false,
+  type = "button",
 }: {
-  onClick: () => void;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   children: React.ReactNode;
   isSelected?: boolean;
+  type?: "button" | "submit" | "reset";
 }) {
+  const selectedClass = isSelected ? "selected" : "";
   return (
-    <button onClick={() => { 
-      onClick?.();            // run onClick function if it exists
-      
-    }} className={`button ${isSelected ? "selected" : ""}`}
-      >
+    <button
+      type={type}
+      onClick={(event) =>  onClick?.(event) }
+      className={`${selectedClass}`}
+    >
       {children}
     </button>
   );
