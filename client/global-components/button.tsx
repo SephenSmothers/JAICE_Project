61092c9d65790ed1ac51408f9b408d0639c23195
djwilson7 <dontai.j.wilson@gmail.com>
2025-10-20 14:54:11 -0500

    This commit contains all of the import update for the files inside of client.
    In this process I isolated updates strictly to the imports and updating them to reference the flatter directory structure.

---
+++ client/global-components/button.tsx
@@ -0,0 +1,25 @@
+// import { localfiles } from "@/directory/path/to/localimport";
+import React from "react";
+
+export default function Button({
+  onClick,
+  children,
+  isSelected = false,
+  type = "button",
+}: {
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
+  isSelected?: boolean;
+  type?: "button" | "submit" | "reset";
+}) {
+  const selectedClass = isSelected ? "selected" : "";
+  return (
+    <button
+      type={type}
+      onClick={(event) =>  onClick?.(event) }
+      className={`${selectedClass}`}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ client/pages/settings/accessibility/accessibility.meta.tsx
@@ -0,0 +1,8 @@
+// import { localfiles } from "@/directory/path/to/localimport";
+
+import { AccessibilityPage } from "@/pages/settings/accessibility/AccessibilityPage";
+
+export const AccessibilityRoute = {
+  path: "/settings/accessibility",
+  element: <AccessibilityPage />,
+};
