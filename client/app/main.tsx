ad8d49eeef24d13a25dfce06d4518af0e7bca5f2
djwilson7 <dontai.j.wilson@gmail.com>
2025-10-20 14:42:35 -0500

    These files are marked as deleted by git, but they have been relocated into the flatter directory structure.
    Now client lives at the root level.

---
+++ client/app/main.tsx
@@ -0,0 +1,18 @@
+// import { localfiles } from "@/directory/path/to/localimport";
+import "@/global-style/Global.css"; // Global CSS style to be injected at app entry point for consistency across all pages.
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "@/global-services/router";
+import AuthProvider from "@/global-components/AuthProvider";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
+    <React.StrictMode>
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    </React.StrictMode>
+);
--- src/client/pages/auth-about/authabout.meta.tsx
+++
@@ -1,6 +0,0 @@
-import { AuthAboutPage } from "@/client/pages/auth-about/AuthAboutPage";
-
-export const AuthAboutRoute = {
-  path: "/auth-about",
-  element: <AuthAboutPage />,
-};
