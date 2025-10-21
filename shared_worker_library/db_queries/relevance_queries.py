c9857f35024978dceeb44622d14841accada141e
djwilson7 <dontai.j.wilson@gmail.com>
2025-10-21 16:47:45 -0500

    Set up the relevance model pipeline. We now have gmail ingestion from OAuth -> relevance model ouptut with db updates inbetween.

---
+++ shared_worker_library/db_queries/relevance_queries.py
@@ -0,0 +1,57 @@
+from shared_worker_library.database import get_connection
+from common.logger import get_logger
+from psycopg.rows import dict_row
+from shared_worker_library.utils.task_definitions import EmailStatus, RelevanceModelResult
+
+logging = get_logger()
+
+
+def get_encrypted_emails(trace_id: str, row_ids: list[int]):
+    logging.info(f"[{trace_id}] Getting encrypted emails for row IDs: {row_ids}")
+    with get_connection() as conn:
+        with conn.cursor(row_factory=dict_row) as cur:
+            cur.execute(
+                "SELECT id, subject_enc, sender_enc, body_enc FROM internal_staging.email_staging WHERE id = ANY(%s)",
+                (row_ids,),
+            )
+            results = cur.fetchall()
+            logging.info(f"[{trace_id}] Retrieved {len(results)} encrypted emails")
+            return results
+
+def update_staging_table(trace_id: str, model_results: RelevanceModelResult):
+    logging.info(f"Updating staging table for trace_id {trace_id}")
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            for item in model_results.relevant:
+                cur.execute(
+                    "UPDATE internal_staging.email_staging SET status = %s WHERE id = %s",
+                    (EmailStatus.AWAIT_CLASSIFICATION.value, item["email_id"]),
+                )
+            for item in model_results.retry:
+                cur.execute(
+                    "UPDATE internal_staging.email_staging SET status = %s WHERE id = %s",
+                    (EmailStatus.RETRY.value, item["email_id"]),
+                )
+            for item in model_results.purge:
+                cur.execute(
+                    "UPDATE internal_staging.email_staging SET status = %s WHERE id = %s",
+                    (EmailStatus.PURGE.value, item["email_id"]),
+                )
+        conn.commit()
+    logging.info(f"Staging table updated for trace_id {trace_id}")
+    return {"status": "updated"}
+
+def update_staging_table_failure(trace_id: str, row_ids: list[int]):
+    logging.info(
+        f"Updating staging table to FAILED_PERMANENTLY for trace_id {trace_id}"
+    )
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            for row_id in row_ids:
+                cur.execute(
+                    "UPDATE internal_staging.email_staging SET status = %s WHERE id = %s",
+                    (EmailStatus.FAILED_PERMANENTLY.value, row_id),
+                )
+        conn.commit()
+    logging.info(f"Staging table updated to FAILED_PERMANENTLY for trace_id {trace_id}")
+    return {"status": "updated_to_failed_permanently"}
