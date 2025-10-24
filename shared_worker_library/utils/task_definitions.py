413309700797aea9aec1df5003ae5bedf9d7c676
djwilson7 <dontai.j.wilson@gmail.com>
2025-10-23 19:11:32 -0500

    Classification Template and NER Template added.

--- shared_worker_library/utils/task_definitions.py
+++ shared_worker_library/utils/task_definitions.py
@@ -10,6 +10,11 @@ class TaskType(Enum):
         "classification_model.classification_task",
         "classification_model_queue",
     )
+    NER_MODEL = ("ner_model.ner_task", "ner_model_queue")
+    TRANSFER_FROM_STAGING = (
+        "staging_to_job_apps",
+        "staging_to_job_apps_queue",
+    )
 
     def __init__(self, task_name: str, queue_name: str):
         self.task_name = task_name
@@ -22,10 +27,27 @@ class EmailStatus(Enum):
     PURGE = "PURGE"
     AWAIT_CLASSIFICATION = "AWAIT_CLASSIFICATION"
     FAILED_PERMANENTLY = "NOT_PROCESSABLE"
+    AWAIT_TRANSFER = "AWAIT_TRANSFER"
 
+class EmailStage(Enum):
+    APPLIED = "Applied"
+    INTERVIEW = "Interview"
+    OFFER = "Offer"
+    ACCEPTED = "Accepted"
+    REJECTED = "Rejected"
 
 @dataclass
 class RelevanceModelResult:
     relevant: list
     retry: list
     purge: list
+
+
+@dataclass
+class ClassificationModelResult:
+    applied: list
+    interview: list
+    offer: list
+    accepted: list
+    rejected: list
+    retry: list
